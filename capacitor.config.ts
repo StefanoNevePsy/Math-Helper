4cfe97f7572617f8883d6822fea855dadbb46c1a
StefanoNevePsy <nevestefanopsy@gmail.com>
2026-03-29 18:48:34 +0200

    Aggiunto Capacitor Android Build e generazione icone Adattive/Monet

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.stefanonevepsy.mathhelper',
+  appName: 'Math Helper',
+  webDir: 'dist'
+};
+
+export default config;
